e58040d08a957843a10ba3c6262ea292421556ac
Mariam Khatoon <mariamkhatoon996@gmail.com>
2023-11-04 22:28:42 +0400

    added typeScript to react app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
-import logo from './logo.svg'
 import './App.css'
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   )
 }
---
+++ client/src/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+
+const rootElement = document.getElementById('root')
+let root
+
+if (rootElement) {
+  root = ReactDOM.createRoot(rootElement)
+} else {
+  console.error("Element with ID 'root' not found.")
+}
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
