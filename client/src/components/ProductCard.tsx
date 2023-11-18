6a7343d24292d07406ecd2deb8b76580bd817d6c
Mariam Khatoon <mariamkhatoon996@gmail.com>
2023-11-18 14:57:24 +0400

    product card made more dynamic

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -15,16 +15,18 @@ const ProductCard: React.FC<ProductCardProps> = ({ height, editable }) => {
       <div className="py-5 flex flex-col items-center rounded-md font-noto hover:cursor-pointer">
         {/* 4:5 ratio */}
         <div
-          className={`h-40 mb-3 md:h-${height} md:w-${height-8} flex items-center justify-center overflow-hidden`}
+          className={`h-40 mb-3 md:h-${height} md:w-${
+            height - 8
+          } flex items-center justify-center overflow-hidden`}
         >
           <img className="object-cover w-full h-full" src="/apple.png" alt="" />
         </div>
         <div>
-          <h4 className={`truncate w-${height-8}`}>
+          <h4 className={`truncate w-${height - 8}`}>
             Apples, Silicon Oasiskm frkn
           </h4>
           <h6 className="text-xs">Mariam Khatoon</h6>
-          <div className="flex gap-3 justify-between  items-end">
+          <div className="flex justify-between items-end">
             <div>
               <div className="flex">
                 <p className="text-xs py-0.5 pr-1">AED</p>
@@ -40,10 +42,16 @@ const ProductCard: React.FC<ProductCardProps> = ({ height, editable }) => {
               />
             </div>
             {editable && (
-              <div className="float-right flex gap-1.5">
-                <EditIcon fontSize="small" />
-                <DeleteIcon fontSize="small" />
-                <VisibilityOffIcon fontSize="small" />
+              <div className="float-right flex mb-2">
+                <div className="rounded-md py-0.5 px-1 hover:bg-gray-200">
+                  <EditIcon fontSize="small" />
+                </div>
+                <div className="rounded-md py-0.5 px-1 hover:bg-gray-200">
+                  <DeleteIcon fontSize="small" />
+                </div>
+                <div className="rounded-md py-0.5 px-1 hover:bg-gray-200">
+                  <VisibilityOffIcon fontSize="small" />
+                </div>
               </div>
             )}
           </div>
