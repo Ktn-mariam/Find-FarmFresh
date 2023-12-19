7dd628d6477d5834179710f2a59559c0b5f4af47
Mariam Khatoon <mariamkhatoon996@gmail.com>
2023-12-19 14:48:39 +0400

    fix: explicit addition of value id removed from Comment

--- server/models/comment.ts
+++ server/models/comment.ts
@@ -1,10 +1,6 @@
 import mongoose from 'mongoose'
 
 const CommentSchema = new mongoose.Schema({
-  _id: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-  },
   userID: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Consumer',
