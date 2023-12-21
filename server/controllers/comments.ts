b414794ff9604fbf193f17881771403ef1b0082e
Mariam Khatoon <mariamkhatoon996@gmail.com>
2023-12-21 20:04:55 +0400

    feat: noOfcomments route also added to display the no of pages

--- server/controllers/comments.ts
+++ server/controllers/comments.ts
@@ -4,28 +4,45 @@ import { StatusCodes } from 'http-status-codes'
 
 const getCommentsOfFarmer = async (req: Request, res: Response) => {
   const { farmerID } = req.params
-  let result = Comment.find({ farmerID })
 
   const limit = 3
-  const page = Number(req.query.page) - 3 || 1
+  const page = Number(req.query.page) - 2 || 1
   const skip = (page - 1) * limit
 
-  result = result.skip(skip).limit(limit)
-  const comments = await result
+  let result = Comment.find({ farmerID }).skip(skip).limit(limit)
+  const comments = await result.exec()
   res.status(StatusCodes.OK).json({ comments })
+}
+
+const getNoOfCommentsOfFarmer = async (req: Request, res: Response) => {
+  const { farmerID } = req.params
+  let result = Comment.find({ farmerID })
+  const noOfComments = (await result.exec()).length
+  res.status(StatusCodes.OK).json({ count: noOfComments })
 }
 
 const getCommentsOfProduct = async (req: Request, res: Response) => {
   const { productID } = req.params
-  let result = Comment.find({ productID })
 
   const limit = 3
-  const page = Number(req.query.page) - 3 || 1
+  const page = Number(req.query.page) - 2 || 1
   const skip = (page - 1) * limit
 
-  result = result.skip(skip).limit(limit)
-  const comments = await result
+  let result = Comment.find({ productID }).skip(skip).limit(limit)
+  const comments = await result.exec()
   res.status(StatusCodes.OK).json({ comments })
 }
 
-export { getCommentsOfFarmer, getCommentsOfProduct }
+const getNoOfCommentsOfProduct = async (req: Request, res: Response) => {
+  const { productID } = req.params
+  let result = Comment.find({ productID })
+  const noOfComments = (await result.exec()).length
+  res.status(StatusCodes.OK).json({ count: noOfComments })
+}
+
+export {
+  getCommentsOfFarmer,
+  getNoOfCommentsOfFarmer,
+  getCommentsOfProduct,
+  getNoOfCommentsOfProduct,
+}
--- server/routes/comments.ts
+++ server/routes/comments.ts
@@ -3,9 +3,13 @@ const router = express.Router()
 import {
   getCommentsOfFarmer,
   getCommentsOfProduct,
+  getNoOfCommentsOfFarmer,
+  getNoOfCommentsOfProduct,
 } from '../controllers/comments'
 
+router.get('/farmer/:farmerID/count', getNoOfCommentsOfFarmer)
 router.get('/farmer/:farmerID', getCommentsOfFarmer)
+router.get('/product/:productID/count', getNoOfCommentsOfProduct)
 router.get('/product/:productID', getCommentsOfProduct)
 
 export default router
