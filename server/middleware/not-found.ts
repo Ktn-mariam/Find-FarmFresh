56658712b84756c8efb430f38189b67e05dd609e
Mariam Khatoon <mariamkhatoon996@gmail.com>
2023-12-21 20:02:42 +0400

    fix: instead of sending a string, a json message is sent when route does not exist

--- server/middleware/not-found.ts
+++ server/middleware/not-found.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 
 const notFound = (req: Request, res: Response) => {
-  res.status(404).send('Route does not exist')
+  res.status(404).json({ msg: 'Route does not exist' })
 }
 
 export default notFound
