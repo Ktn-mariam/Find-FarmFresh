68096879185b70d65922d6ebfb3e3e7c8d57914e
Mariam Khatoon <mariamkhatoon996@gmail.com>
2023-12-27 22:59:43 +0400

    feat: Shopping cart functionality successfully implemented, items can now be added, updated, deleted or checked out for order in the cart

--- client/src/context/authentication.tsx
+++ client/src/context/authentication.tsx
@@ -1,6 +1,15 @@
 import React, { createContext, useEffect, useState } from 'react'
 import { Comment } from '../types/Comment'
 import { Role } from '../types/Auth'
+import { CartItem } from '../types/Order'
+
+interface AddToCartItemType {
+  farmerID: string
+  farmerName: string
+  productID: string
+  quantity: number
+  productPrice: number
+}
 
 export interface UserProfileType {
   locationCoordinates?: {
@@ -33,6 +42,13 @@ export interface UserProfileType {
   role?: 'Farmer' | 'Consumer'
   loggedIn: boolean
   // ORDERS
+  cart?: CartItem[]
+}
+
+interface UpdateQuantityItemType {
+  farmerID: string
+  productID: string
+  quantity: number
 }
 
 interface AuthenticationContextType {
@@ -40,6 +56,18 @@ interface AuthenticationContextType {
   loadingLogInData: boolean
   setLogInData: React.Dispatch<React.SetStateAction<UserProfileType>>
   logInError: boolean
+  handleAddToCart: (addToCartItem: AddToCartItemType) => void
+  updateQuantityInCart: (updateQuantityItem: UpdateQuantityItemType) => void
+  updateTotalPrice: (updateTotalPrice: {
+    farmerID: string
+    totalPrice: number
+  }) => void
+  deleteItemFromCart: (deleteItem: {
+    farmerID: string
+    productID: string
+  }) => void
+  deleteAllItemsFromCartofFarmer: (farmerID: string) => void
+  checkOutAllHandler: () => Promise<void>
 }
 
 const AuthenticationContext = createContext<AuthenticationContextType>({
@@ -49,6 +77,12 @@ const AuthenticationContext = createContext<AuthenticationContextType>({
   loadingLogInData: true,
   setLogInData: () => {},
   logInError: false,
+  handleAddToCart: () => {},
+  updateQuantityInCart: () => {},
+  updateTotalPrice: () => {},
+  deleteItemFromCart: () => {},
+  deleteAllItemsFromCartofFarmer: () => {},
+  checkOutAllHandler: async () => {},
 })
 
 interface AuthenticationContextProviderPropsType {
@@ -60,7 +94,6 @@ export const AuthenticationContextProvider: React.FC<AuthenticationContextProvid
 }) => {
   const token = localStorage.getItem('token')
 
-  console.log(`TOKEN: ${token}`)
   const [loadingLogInData, setLoadingLogInData] = useState(true)
   const [logInData, setLogInData] = useState<UserProfileType>({
     loggedIn: false,
@@ -97,6 +130,8 @@ export const AuthenticationContextProvider: React.FC<AuthenticationContextProvid
           loginInfo.comments = userData.user.userDetail[0].comments
           loginInfo.farmerRating = userData.user.userDetail[0].farmerRating
           loginInfo.description = userData.user.userDetail[0].description
+        } else {
+          loginInfo.cart = userData.user.userDetail[0].cart
         }
         setLogInData(loginInfo)
       } else {
@@ -109,11 +144,202 @@ export const AuthenticationContextProvider: React.FC<AuthenticationContextProvid
     fetchData()
   }, [token])
 
+  const handleAddToCart = (addToCartItem: AddToCartItemType) => {
+    const logInDataPrev = logInData
+    const existingFarmerIndex = logInData.cart?.findIndex((farmer) => {
+      return farmer.farmerID === addToCartItem.farmerID
+    })!
+    if (logInDataPrev.cart) {
+      if (existingFarmerIndex !== -1 && logInDataPrev.cart) {
+        const existingProductIndex = logInDataPrev.cart[
+          existingFarmerIndex
+        ].products.findIndex((product) => {
+          return product.productID === addToCartItem.productID
+        })
+
+        if (existingProductIndex !== -1) {
+          logInDataPrev.cart[existingFarmerIndex].products[
+            existingProductIndex
+          ].quantity += addToCartItem.quantity
+        } else {
+          logInDataPrev.cart[existingFarmerIndex].products.unshift({
+            productID: addToCartItem.productID,
+            quantity: addToCartItem.quantity,
+          })
+        }
+        logInDataPrev.cart[existingFarmerIndex].totalPrice =
+          logInDataPrev.cart[existingFarmerIndex].totalPrice +
+          addToCartItem.productPrice * addToCartItem.quantity
+        setLogInData(logInDataPrev)
+      } else {
+        const cartItem = {
+          farmerID: addToCartItem.farmerID,
+          farmerName: addToCartItem.farmerName,
+          totalPrice: addToCartItem.productPrice,
+          products: [
+            {
+              productID: addToCartItem.productID,
+              quantity: addToCartItem.quantity,
+            },
+          ],
+        }
+
+        logInDataPrev.cart.unshift(cartItem)
+        setLogInData(logInDataPrev)
+        updateCartForConsumer()
+      }
+    }
+  }
+
+  const updateCartForConsumer = async () => {
+    const token = localStorage.getItem('token')
+    const parsedToken = JSON.parse(token!)
+
+    const cart = logInData.cart
+    const consumerResponse = await fetch(
+      `http://localhost:5000/api/v1/consumers`,
+      {
+        method: 'PATCH',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${parsedToken}`,
+        },
+        body: JSON.stringify({ cart }),
+      },
+    )
+
+    const consumerData = await consumerResponse.json()
+    console.log(consumerData)
+  }
+
+  const updateQuantityInCart = (updateQuantityItem: UpdateQuantityItemType) => {
+    console.log(updateQuantityItem.quantity)
+
+    const logInDataPrev = logInData
+    if (logInDataPrev.cart) {
+      const existingFarmerIndex = logInDataPrev.cart?.findIndex((farmer) => {
+        return farmer.farmerID === updateQuantityItem.farmerID
+      })!
+
+      const existingProductIndex = logInDataPrev.cart[
+        existingFarmerIndex
+      ].products.findIndex((product) => {
+        return product.productID === updateQuantityItem.productID
+      })
+
+      logInDataPrev.cart[existingFarmerIndex].products[
+        existingProductIndex
+      ].quantity = updateQuantityItem.quantity
+
+      setLogInData(logInDataPrev)
+      updateCartForConsumer()
+    }
+  }
+
+  const updateTotalPrice = (updatePrice: {
+    farmerID: string
+    totalPrice: number
+  }) => {
+    const logInDataPrev = logInData
+    if (logInDataPrev.cart) {
+      const existingFarmerIndex = logInDataPrev.cart?.findIndex((farmer) => {
+        return farmer.farmerID === updatePrice.farmerID
+      })
+
+      logInDataPrev.cart[existingFarmerIndex].totalPrice =
+        updatePrice.totalPrice
+
+      setLogInData(logInDataPrev)
+      updateCartForConsumer()
+    }
+  }
+
+  const deleteItemFromCart = (deleteItem: {
+    farmerID: string
+    productID: string
+  }) => {
+    const logInDataPrev = logInData
+    if (logInDataPrev.cart) {
+      const existingFarmerIndex = logInDataPrev.cart?.findIndex((farmer) => {
+        return farmer.farmerID === deleteItem.farmerID
+      })
+
+      const existingProductIndex = logInDataPrev.cart[
+        existingFarmerIndex
+      ].products.findIndex((product) => {
+        return product.productID === deleteItem.productID
+      })
+
+      logInDataPrev.cart[existingFarmerIndex].products.splice(
+        existingProductIndex,
+        1,
+      )
+
+      if (logInDataPrev.cart[existingFarmerIndex].products.length === 0) {
+        logInDataPrev.cart.splice(existingFarmerIndex, 1)
+      }
+
+      setLogInData(logInDataPrev)
+      updateCartForConsumer()
+    }
+  }
+
+  const deleteAllItemsFromCartofFarmer = (farmerID: string) => {
+    const logInDataPrev = logInData
+    if (logInDataPrev.cart) {
+      const existingFarmerIndex = logInDataPrev.cart?.findIndex((farmer) => {
+        return farmer.farmerID === farmerID
+      })
+      logInDataPrev.cart.splice(existingFarmerIndex, 1)
+      setLogInData(logInDataPrev)
+      updateCartForConsumer()
+    }
+  }
+
+  const checkOutAllHandler = async () => {
+    const token = localStorage.getItem('token')
+    const parsedToken = JSON.parse(token!)
+
+    if (logInData.cart) {
+      const orders = await Promise.all(
+        logInData.cart.map(async (cartItem) => {
+          const orderResponse = await fetch(
+            'http://localhost:5000/api/v1/orders',
+            {
+              method: 'POST',
+              mode: 'cors',
+              headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${parsedToken}`,
+              },
+              body: JSON.stringify(cartItem),
+            },
+          )
+
+          const orderData = await orderResponse.json()
+          console.log(orderData)
+        }),
+      )
+      console.log(orders)
+      const logInDataPrev = logInData
+      logInDataPrev.cart = []
+      setLogInData(logInDataPrev)
+      updateCartForConsumer()
+    }
+  }
+
   let contextValue = {
     logInData,
     loadingLogInData,
     setLogInData,
     logInError,
+    handleAddToCart,
+    updateQuantityInCart,
+    updateTotalPrice,
+    deleteItemFromCart,
+    deleteAllItemsFromCartofFarmer,
+    checkOutAllHandler,
   }
 
   return (
