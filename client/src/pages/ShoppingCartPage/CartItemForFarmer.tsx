baaa1f1025ae026c848f6366c6320cfae20c5cbc
Mariam Khatoon <mariamkhatoon996@gmail.com>
2023-12-27 22:57:56 +0400

    feat: Shopping cart functionality successfully implemented, items can now be added, updated, deleted or checked out for order in the cart

---
+++ client/src/pages/ShoppingCartPage/CartItemForFarmer.tsx
@@ -0,0 +1,145 @@
+import React, { useContext, useEffect, useState } from 'react'
+import ProductCart from '../../components/ProductCart'
+import DeleteIcon from '@mui/icons-material/Delete'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import { CartItem } from '../../types/Order'
+import { NavLink } from 'react-router-dom'
+import { ProductDetailForOrder } from '../../types/Product'
+import AuthenticationContext from '../../context/authentication'
+
+interface CartItemForFarmerPropsType {
+  cartItem: CartItem
+  findTotalPriceAndItems: () => void
+}
+
+const CartItemForFarmer: React.FC<CartItemForFarmerPropsType> = ({
+  cartItem,
+  findTotalPriceAndItems,
+}) => {
+  const { updateTotalPrice, deleteAllItemsFromCartofFarmer } = useContext(
+    AuthenticationContext,
+  )
+  const [products, setProducts] = useState<ProductDetailForOrder[] | null>()
+  const [refetchProducts, setRefetchProducts] = useState(false)
+  const [displayCartItem, setDisplayCartItem] = useState(true)
+
+  useEffect(() => {
+    let totalPrice = 0
+    const fetchProductDetails = async () => {
+      const productDetails = await Promise.all(
+        cartItem.products.map(async (product) => {
+          const productDetailResponse = await fetch(
+            `http://localhost:5000/api/v1/products/orderDetail/${product.productID}`,
+          )
+          const productDetailData = await productDetailResponse.json()
+
+          console.log(productDetailData.product.price)
+
+          totalPrice += productDetailData.product.price * product.quantity
+
+          return {
+            ...productDetailData.product,
+            quantity: product.quantity,
+          }
+        }),
+      )
+
+      setProducts(productDetails)
+      updateTotalPrice({ farmerID: cartItem.farmerID, totalPrice })
+      findTotalPriceAndItems()
+    }
+
+    fetchProductDetails()
+
+    if (refetchProducts) {
+      fetchProductDetails()
+      setRefetchProducts(false)
+    }
+  }, [
+    cartItem.products,
+    cartItem.farmerID,
+    updateTotalPrice,
+    refetchProducts,
+    findTotalPriceAndItems,
+  ])
+
+  const addOrderHandler = async () => {
+    const token = localStorage.getItem('token')
+    const parsedToken = JSON.parse(token!)
+
+    const orderResponse = await fetch(`http://localhost:5000/api/v1/orders`, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${parsedToken}`,
+      },
+      body: JSON.stringify(cartItem),
+    })
+
+    const orderData = await orderResponse.json()
+    console.log(orderData)
+
+    deleteAllItemsFromCartofFarmer(cartItem.farmerID)
+    setDisplayCartItem(false)
+  }
+
+  if (!displayCartItem) {
+    return <div></div>
+  }
+
+  return (
+    <div className="flex font-workSans flex-col gap-2">
+      <div className="flex items-center justify-between">
+        <NavLink to={`farmer=profile/${cartItem.farmerID}`}>
+          <div className="text-xl font-bold">{cartItem.farmerName}</div>
+        </NavLink>
+        <button
+          className="flex items-center gap-1"
+          onClick={() => {
+            deleteAllItemsFromCartofFarmer(cartItem.farmerID)
+            setDisplayCartItem(false)
+          }}
+        >
+          <DeleteIcon fontSize="small" />
+          <div className="text-md font-bold">Delete Items</div>
+        </button>
+      </div>
+      <div className="flex gap-5 p-5 rounded-lg border border-gray-300">
+        <div className="w-3/4">
+          {products && products.length < 4 && (
+            <div className="flex flex-col gap-1">
+              {products.map((product) => {
+                return (
+                  <ProductCart
+                    isShoppingCart={true}
+                    product={product}
+                    setRefetchProducts={setRefetchProducts}
+                  />
+                )
+              })}
+            </div>
+          )}
+        </div>
+        <div className="w-1/4 pr-2 pb-3 flex my-3 items-end justify-between flex-col">
+          <div className="flex flex-col items-end">
+            <h1 className="text-lg font-bold">Total:</h1>
+            <div className="flex items-center text-xl">
+              <p className="text-md pr-1">AED</p>
+              <p className="font-bold text-red-700">{cartItem.totalPrice}</p>
+            </div>
+          </div>
+          <button
+            onClick={addOrderHandler}
+            className="px-3 py-1 bg-yellow-400 rounded-lg"
+          >
+            Checkout
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CartItemForFarmer
--- client/src/pages/ShoppingCartPage/index.tsx
+++ client/src/pages/ShoppingCartPage/index.tsx
@@ -1,147 +1,73 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import StoreNavbar from '../../components/StoreNavbar'
-import ProductCart from '../../components/ProductCart'
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight'
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft'
-import DeleteIcon from '@mui/icons-material/Delete'
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+import CartItemForFarmer from './CartItemForFarmer'
+import AuthenticationContext from '../../context/authentication'
 
-interface ArrowProps {
-  onClick?: React.MouseEventHandler<HTMLButtonElement>
-  style?: React.CSSProperties
-}
-
-const PrevArrow: React.FC<ArrowProps> = ({ onClick, style }) => {
-  return (
-    <button
-      className="absolute -left-3 top-1/2 transform -translate-y-1/2 z-10"
-      title="left"
-      onClick={onClick}
-    >
-      <KeyboardArrowLeftIcon />
-    </button>
-  )
-}
-const NextArrow: React.FC<ArrowProps> = ({ onClick, style }) => {
-  return (
-    <button
-      className="absolute -right-4 top-1/2 transform -translate-y-1/2"
-      title="right"
-      onClick={onClick}
-    >
-      <KeyboardArrowRightIcon />
-    </button>
-  )
-}
 const ShoppingCartPage = () => {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 3,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true,
-        },
-      },
-    ],
+  const { logInData, checkOutAllHandler } = useContext(AuthenticationContext)
+  const [totalPrice, setTotalPrice] = useState(0)
+  const [totalItems, setTotalItems] = useState(0)
+  const [displayNoProducts, setDisplayNoProducts] = useState(true)
+
+  const findTotalPriceAndItems = () => {
+    const sumPrice =
+      logInData.cart?.reduce((sum, cartItem) => sum + cartItem.totalPrice, 0) ||
+      0
+    setTotalPrice(sumPrice)
+
+    const sumItems =
+      logInData.cart?.reduce(
+        (sum, cartItem) => sum + cartItem.products.length,
+        0,
+      ) || 0
+    setTotalItems(sumItems)
   }
 
-  const orders = [1, 2, 3, 4, 5]
   return (
-    <div>
+    <div className="font-workSans">
       <StoreNavbar />
-      <div className="md:px-36 px-14 py-5 text-xs md:text-sm font-noto mb-40">
+      <div className="md:px-36 px-14 py-5 text-xs md:text-sm mb-40">
         <h1 className="text-2xl font-bold mb-6 mt-3">Shopping Cart</h1>
         <div className="flex gap-10">
-          <div className="flex flex-col gap-10 w-2/3">
-            {orders.map((order) => {
-              return (
-                <div className="flex flex-col gap-2">
-                  <div className="flex items-center justify-between">
-                    <div className="text-xl font-bold">Mariam Khatoon</div>
-                    <div className="flex items-center gap-1 hover:cursor-pointer">
-                      <DeleteIcon fontSize="small" />
-                      <div className="text-md font-bold">Delete Order</div>
-                    </div>
-                  </div>
-                  <div className="py-3 px-5 bg-gray-100 flex gap-5">
-                    <div className="w-3/4">
-                      <Slider {...settings}>
-                        <ProductCart isShoppingCart={true} />
-                        <ProductCart isShoppingCart={true} />
-                        <ProductCart isShoppingCart={true} />
-                        <ProductCart isShoppingCart={true} />
-                        <ProductCart isShoppingCart={true} />
-                        <ProductCart isShoppingCart={true} />
-                      </Slider>
-                    </div>
-                    <div className="w-1/4 pr-2 pb-3 flex my-3 items-end justify-between flex-col">
-                      <div className="flex flex-col items-end">
-                        <h1 className="text-lg font-bold">Total:</h1>
-                        <div className="flex items-center text-xl">
-                          <p className="text-md pr-1">AED</p>
-                          <p className="font-bold text-red-700">35.50</p>
-                        </div>
-                      </div>
-                      <button className="px-3 py-1 bg-yellow-400 rounded-lg">
-                        Checkout
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              )
-            })}
-          </div>
+          {displayNoProducts ? (
+            <div className="flex flex-col gap-10 w-2/3">
+              {logInData.cart ? (
+                logInData.cart.map((cartItem) => {
+                  return (
+                    <CartItemForFarmer
+                      cartItem={cartItem}
+                      findTotalPriceAndItems={findTotalPriceAndItems}
+                    />
+                  )
+                })
+              ) : (
+                <div>Please sign in/up to add items</div>
+              )}
+            </div>
+          ) : (
+            <div className="w-2/3"></div>
+          )}
           <div className="w-1/3">
             <div className="px-10 py-10 border border-1 border-zinc-300 rounded-2xl">
               <div className="text-lg font-bold mb-5">Order Summary</div>
               <div className="flex items-center justify-between">
                 <div>Total Item(s):</div>
-                <div className="font-bold">9 Items</div>
-              </div>
-              <div className="flex items-center justify-between mt-3">
-                <div>Total Product(s) Price:</div>
-                <div className="font-bold">AED 87.34</div>
-              </div>
-              <div className="flex items-center justify-between mt-3">
-                <div>Total Delivery Price:</div>
-                <div className="font-bold">AED 5.45</div>
+                <div className="font-bold">{totalItems} Items</div>
               </div>
               <div className="border-t border-gray-300 flex items-center b justify-between mt-4">
-                <div className="mt-4">Total:</div>
-                <div className="font-bold mt-4 text-xl">AED 90.45</div>
+                <div className="mt-4">Total Price:</div>
+                <div className="font-bold mt-4 text-xl">AED {totalPrice}</div>
               </div>
-              <button className="w-full py-2 mt-8 bg-yellow-400 rounded-lg">
-                Checkout
+              <button
+                onClick={() => {
+                  checkOutAllHandler()
+                  setDisplayNoProducts(false)
+                  setTotalItems(0)
+                  setTotalPrice(0)
+                }}
+                className="w-full py-2 mt-8 bg-yellow-400 rounded-lg"
+              >
+                Checkout All
               </button>
             </div>
           </div>
