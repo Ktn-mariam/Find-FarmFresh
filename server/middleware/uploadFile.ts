216e22015b343d91a9a29500cbf513a818473774
Mariam Khatoon <mariamkhatoon996@gmail.com>
2024-08-18 20:35:30 +0530

    feat: upload file middleware changed so that we can control how files are stored in the disk

--- server/middleware/uploadFile.ts
+++ server/middleware/uploadFile.ts
@@ -1,3 +1,14 @@
 import multer from 'multer'
-const upload = multer({ dest: 'uploads/' })
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/uploads')
+  },
+  filename: function (req, file, cb) {
+    return cb(null, `${Date.now()}-${file.originalname}`)
+  },
+})
+
+const upload = multer({ storage })
+
 export default upload
