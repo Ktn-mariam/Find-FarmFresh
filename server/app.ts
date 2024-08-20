a48e8722f1cbe87239819685199ef9e0439d0aa5
Mariam Khatoon <mariamkhatoon996@gmail.com>
2024-08-20 18:45:51 +0530

    feat: multer package installed

--- server/app.ts
+++ server/app.ts
@@ -20,6 +20,8 @@ const app = express()
 // middleware
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use('/uploads', express.static('uploads'))
 
 // routes
 app.use('/api/v1/auth', authRouter)
