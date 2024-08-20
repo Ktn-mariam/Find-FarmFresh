8152c1f07a7f945df6cfe5a607c9285d14b51fc0
Mariam Khatoon <mariamkhatoon996@gmail.com>
2024-08-20 18:45:04 +0530

    fix: error fixed in the uploadFile middleware

--- server/middleware/uploadFile.ts
+++ server/middleware/uploadFile.ts
@@ -2,10 +2,10 @@ import multer from 'multer'
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '/uploads')
+    cb(null, './uploads')
   },
   filename: function (req, file, cb) {
-    return cb(null, `${Date.now()}-${file.originalname}`)
+    return cb(null, `${Date.now()}-${req.user.userID}-${file.originalname}`)
   },
 })
 
