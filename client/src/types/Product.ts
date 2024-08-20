0b8ba5e99401da8ca116add7223fce885d35b693
Mariam Khatoon <mariamkhatoon996@gmail.com>
2024-08-20 18:48:53 +0530

    feat: new ProductDetailTypeForDisplay type added

--- client/src/types/Product.ts
+++ client/src/types/Product.ts
@@ -78,6 +78,79 @@ export interface ProductDetailType {
     rating: number
   }
   _id: string
+  images: File
+  title: string
+  price: number
+  parentCategory:
+    | 'fruits'
+    | 'vegetables'
+    | 'coffee&tea'
+    | 'diary&eggs'
+    | 'meat'
+    | 'honey'
+    | 'flowers'
+    | 'driedFruits'
+  category:
+    | 'Bananas'
+    | 'Oranges'
+    | 'Apples'
+    | 'Mangoes'
+    | 'Pinapples'
+    | 'Strawberries'
+    | 'Potatoes'
+    | 'Onions'
+    | 'Tomatoes'
+    | 'Brinjals'
+    | 'Cauliflower'
+    | 'Spinach'
+    | 'Carrots'
+    | 'Tea'
+    | 'Coffee'
+    | 'Milk'
+    | 'Butter'
+    | 'Cheese'
+    | 'Eggs'
+    | 'Goat'
+    | 'Sheep'
+    | 'Chicken'
+    | 'Honey'
+    | 'Beewax'
+    | 'Pollen'
+    | 'Rose'
+    | 'Orchids'
+    | 'Sunflowers'
+    | 'Lillies'
+    | 'Tulips'
+    | 'Dahlia'
+    | 'Pistachios'
+    | 'Apricot'
+    | 'Dates'
+    | 'Cashew'
+    | 'Almonds'
+  farmerID: string
+  farmerName: string
+  isVisible: boolean
+  delivery: boolean
+  organic: boolean
+  transaction: boolean
+  cashOnDelivery: boolean
+  returnableChoice: boolean
+  hasDiscount: boolean
+  onSiteShopping: boolean
+  comments: Comment[]
+}
+export interface ProductDetailTypeForDisplay {
+  productRating: {
+    voteCount: {
+      five: number
+      four: number
+      three: number
+      two: number
+      one: number
+    }
+    rating: number
+  }
+  _id: string
   images: string[]
   title: string
   price: number
