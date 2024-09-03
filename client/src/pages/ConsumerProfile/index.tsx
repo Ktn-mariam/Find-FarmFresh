17a288a557e9a494a57c742194106cc50e437edf
Mariam Khatoon <mariamkhatoon996@gmail.com>
2024-09-03 21:54:59 +0400

    feat: logout button added to consumerprofile

--- client/src/pages/ConsumerProfile/index.tsx
+++ client/src/pages/ConsumerProfile/index.tsx
@@ -1,14 +1,19 @@
 import React, { useContext, useEffect, useState } from 'react'
 import StoreNavbar from '../../components/StoreNavbar'
+import { useNavigate } from 'react-router-dom'
 import ProfileSideBar from '../../components/ProfileSideBar'
 import ReviewsModal from './ReviewsModal'
 import AuthenticationContext from '../../context/authentication'
+import LogoutIcon from '@mui/icons-material/Logout'
 import { OrderType } from '../../types/Order'
 import Order from './Order'
 
 const ConsumerProfile = () => {
+  const navigate = useNavigate()
   const [openReviewModal, setOpenReviewModal] = useState(false)
-  const { logInData, loadingLogInData } = useContext(AuthenticationContext)
+  const { logInData, loadingLogInData, setLogInData } = useContext(
+    AuthenticationContext,
+  )
   const [orders, setOrders] = useState<OrderType[] | null>(null)
 
   useEffect(() => {
@@ -38,11 +43,12 @@ const ConsumerProfile = () => {
     return <div>Loading</div>
   }
 
-  if (!loadingLogInData && !logInData.loggedIn) {
-    return <div>Please log In to view your profile</div>
+  if (!logInData.loggedIn && !loadingLogInData) {
+    navigate('/sign-in')
   }
 
   const profileInformation = {
+    ID: logInData.userID!,
     name: logInData.name!,
     locationCoordinates: logInData.locationCoordinates!,
     image: logInData.image!,
@@ -60,12 +66,24 @@ const ConsumerProfile = () => {
       <div className="md:px-36 px-14 pt-10 mb-32 font-noto">
         <div className="flex">
           <div className="grid grid-cols-3 gap-10">
+            {/* <div className="text-2xl font-bold">My Profile</div> */}
             <ProfileSideBar
               isFarmer={false}
               editable={true}
               profileInformation={profileInformation}
             />
             <div className="col-span-2">
+              <button
+                className="flex items-center gap-2 hover:cursor-pointer float-right"
+                onClick={() => {
+                  localStorage.removeItem('token')
+                  setLogInData({ loggedIn: false })
+                  navigate('/sign-in')
+                }}
+              >
+                <LogoutIcon />
+                <h3 className="text-xl">Logout</h3>
+              </button>
               <h1 className="text-lg font-bold mb-5">My Orders</h1>
               {orders ? (
                 <div className="flex flex-col gap-10">
