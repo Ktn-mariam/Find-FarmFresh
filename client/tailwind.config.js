69aae8fb52163732d2e1836430a4991dea97bff1
Mariam Khatoon <mariamkhatoon996@gmail.com>
2024-09-15 16:19:37 +0400

    feat: tailwind configuration updated

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -27,32 +27,18 @@ module.exports = withMT({
         '110': '470px',
       },
       colors: {
-        lightGrey: '#f8f9fa',
         black: '#1F2421',
         blue: '#216869',
         green: '#49A078',
         'light-green': '#9CC5A1',
-        grey: '#DCE1DE',
-        ashGrey: '#E7EBE9',
-        lightGreen: '#2b9348',
-        mintCream: '#E1F5E7',
-        celadon: '#B7E4C7',
-        // NEW COLOURS I WILL BE USING
         night: '#0D0A0B', // black
-        charcoal: '#454955', //grey
-        coolGray: '#9C9CA5',
-        frenchGray: '#C8C6CD',
-        platinum: '#DEDBE1',
         beige: '#DBE4CD',
         magnolia: '#F3EFF5', //cream
         teaGreen1: '#D3E0BF', //lighest
         teaGreen2: '#C3D8A4',
-        pistachio1: '#B3D089',
-        pistachio2: '#A3C86E',
-        pistachio3: '#93C053',
         appleGreen: '#72B01D',
         asparagus: '#679E1A',
-        officeGreen: '#3F7D20', //darkest
+        neonYellow: '#018749',
       },
       fontFamily: {
         workSans: ['Work Sans', 'sans-serif'],
